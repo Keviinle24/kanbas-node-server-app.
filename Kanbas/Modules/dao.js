9a8fb49e0578626e32ff9eb40a96c8e543533baf
Keviinle24 <156150226+Keviinle24@users.noreply.github.com>
2024-12-08 20:39:12 -0500

    Add files via upload

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -1,19 +1,17 @@
-import Database from "../Database/index.js";
-
-export function createModule(module) {
-  return model.create(module);
-
-  }
-  
+import model from "./model.js";
 export function findModulesForCourse(courseId) {
   return model.find({ course: courseId });
 }
+
+export function createModule(module) {
+  delete module._id;
+  return model.create(module);
+}
+
 export function deleteModule(moduleId) {
   return model.deleteOne({ _id: moduleId });
+}
 
-   }
-   
 export function updateModule(moduleId, moduleUpdates) {
   return model.updateOne({ _id: moduleId }, moduleUpdates);
 }
-  
